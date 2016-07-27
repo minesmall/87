de306ee46052dfaf1467e41bbffe3e47c5578f8b
minesmall <a7474310@gmail.com>
2016-07-27 15:55:33 +0800

    Update reservation.js

--- reservation.js
+++ reservation.js
@@ -1,143 +1,117 @@
 var express = require('express');
 var mongodb = require('mongodb');
 var moment = require('moment');
+var nodemailer = require('nodemailer');
+var crypto = require('crypto');
 var app = express();
-
-var uri = 'mongodb://ching_1214:Sylar1214@ds047612.mongolab.com:47612/dbforitaoke';
-
+var email ;
+var password;
+var username;
+var PhoneNumber;
+var birth;
+var uri = 'mongodb://minesmall:earik2@ds013584.mlab.com:13584/gonow';
 var database;
 
-mongodb.MongoClient.connect(uri, function(err, db) {
-	if (err) {
-		console.log('connect mongo db error ' + err);
-	} else {
+
+var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+    auth: {
+        user: 'a7474310@gmail.com',
+        pass: 'earik2123'
+    }
+});
+mongodb.MongoClient.connect(uri, function(err, db){
+	if(err){
+		console.log('connect mongo db error' + err);
+	}else{
 		console.log('connect mongo db success');
 		database = db;
 	}
 });
+app.get('/api/createDataPoint',function(request,response){
+	email = request.query.email;
+	password = request.query.password;
+	username = request.query.username;
+	PhoneNumber = request.query.PhoneNumber;
+	birth = request.query.birth;
 
-app.get('/api/createDataPoint', function(request, response) {
-	if (!request.query.value) {
-		__sendErrorResponse(response, 403, 'No query parameters value');
-		return;
-	}
-
-	var a;
-	
-	var date;
-	var time;
-	var two;
-	var four;
-	var six;
-		
-	var endString
-	var str = request.query.value;
-	var reservationArray = new Array();
-	var reservationArray = str.split(",");
-	
-	for(a=0; a<5; a++)
-	{
-		if(a==0)
-		{
-			date = reservationArray[a];
+	var items = database.collection('login');
+	var randomstring = crypto.randomBytes(32).toString('base64').substr(0, 8);
+	items.find({email:request.query.email}).toArray(function(err,docs){
+		if (err) {
+			response.type('此信箱已被使用');
+			response.status(406).send(err);
+		} else {
+			var options = {
+				from: 'a7474310@gmail.com',
+ 				to: request.query.email,
+ 				subject: '一起趣玩-共遊跟車系統認證信',
+ 				text: '親愛的用戶您好:歡迎您成為一起趣玩-共遊跟車系統的會員！\n請輸入以下驗證碼:'+ randomstring
+ 			};
+			transporter.sendMail(options,function(error, info){
+			    if(error){
+			        console.log(error);
+			    }else{
+			        console.log('訊息發送: ' + info.response);
+			    }
+			});
+			//response.type('application/json');
+			//response.status(200).send('code:'+randomstring).end();
+			response.write(randomstring);
+			response.end();
 		}
-		else if (a == 1)
-        {
-        	time = reservationArray[a];
-        }
-		else if (a == 2)
-        {
-        	two = reservationArray[a];
-        }
-		else if (a == 3)
-		{
-			four = reservationArray[a];
-		}
-		else if (a == 4)
-   		{
-   			six = reservationArray[a];
- 		}
-       	
-	}
-	
-	
-
+	});
+});
+app.get('/api/insertMdata',function(request,response){
+	var items = database.collection('login');
 	var insert = {
-		date :　date,
-		time : time,
-		two : two,
-		four : four,
-		six : six,
-	};
-
-		
-	var items = database.collection('dbforreservation');
-	items.insert(insert, function(err, result) {
-		if (err) {
-			__sendErrorResponse(response, 406, err);
-		} else {
-			response.type('application/json');
-			response.status(200).send(result);
-			response.end();
-		}
-	});
+		email ,
+		password,
+		username,
+		PhoneNumber,
+		birth
+	}
+	var code = 0;
+	code = request.query.code;
+	if(code == 1){
+		items.insert(insert,function(err,result){
+			if (err) {
+				response.status(406).send(err);
+			} else {
+				response.type('application/json');
+				response.status(200).send(result).end();
+				email='' ;
+				password='';
+				username='';
+				PhoneNumber='';
+				birth='';
+			}
+		});	
+	} else{
+		console.log(error);
+	}
 });
 
-app.get('/api/querySeatData', function(request, response) {
-	var items = database.collection('dbforreservation');
-
-	var limit = parseInt(request.query.limit, 10) || 100;
-
-	items.find().sort({$natural: -1}).limit(limit).toArray(function (err, docs) {
-		if (err) {
+app.get('/api/queryDataPoint',function(request,response){
+	var items = database.collection('login');
+items.find().sort({$natural:-1}).toArray(function(err,docs){
+	if (err) {
 			console.log(err);
-			__sendErrorResponse(response, 406, err);
+			response.status(406).send(err);
 		} else {
-			var jsArray = new Array();
-            var jsArray = docs;
-            var seatCount;
-            for(var i = 0; i < jsArray.length; i++){
-                var jsObj = Object();
-                var jsObj = jsArray[i];
-                seatCount = jsObj.seat;
-            }
 			response.type('application/json');
-			response.status(200).send(seatCount);
+			response.status(200).send(docs);
 			response.end();
+
 		}
 	});
 });
-
-app.get('/api/updateSeatData', function(request, response) {
-	var items = database.collection('dbforreservation');
-	var limit = parseInt(request.query.limit, 10) || 100;
-
-	var str = request.query.value;
-
-	items.update( { title:"CountSeat" }, { '$set': { seat:str } });
-	response.type('application/json');
-	response.status(200).send("succeed");
-	response.end();
-});
-
-
-
-app.use(express.static(__dirname + '/public'));
-
+	app.use(express.static(__dirname+'/public'));
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 });
 
-app.listen(process.env.PORT || 6666);
-console.log('port ' + (process.env.PORT || 6666));
-
-function __sendErrorResponse(response, code, content) {
-	var ret = {
-		err: code,
-		desc : content 
-	};
-	response.status(code).send(ret);
-	response.end();
-}
+app.listen(process.env.PORT || 5000);
+console.log('port ' + (process.env.PORT || 5000));
